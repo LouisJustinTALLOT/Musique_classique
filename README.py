9baf42b9a2a9be3f01a9a7b41a990500f8370dd9
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-11-15 19:33:51 +0100

    on ajoute les noms des compositeurs dans un tableau

--- README.py
+++ README.py
@@ -6,26 +6,29 @@ fichiers = set(os.listdir())
 
 fichiers.remove(".git")
 fichiers.remove("README.py")
+fichiers.remove("README.md")
 fichiers.remove("template.md")
 
-liste_fichiers = set()
+liste_fichiers = []
 
 for i in fichiers : 
-    print(i)
-    liste_fichiers.add(i[:-3])
+    liste_fichiers.append(i[:-3])
     
 
-print(fichiers)
-print(liste_fichiers)
-liste_fichiers = list(liste_fichiers)
 liste_fichiers.sort()
-print(liste_fichiers)
 
 
 texte = f"""# Musique classique
 
 {len(liste_fichiers)} compositeurs sont répertoriés dans cette base.
-"""
+
+|Nom du compositeur |
+|-------------------|"""
+
+for comp in liste_fichiers : 
+    texte+= f"""\n|[{comp}](https://github.com/LouisJustinTALLOT/compositeurs/blob/master/{comp}.md)|"""
+
+
 
 with open('README.md','w', encoding='utf8') as file :
     
