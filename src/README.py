7c0f355860d17c089e30d46c7baecd859f83dbe2
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-11-15 19:51:32 +0100

    Adaptation à la réorganisation du répo

--- src/README.py
+++ src/README.py
@@ -2,12 +2,23 @@
 
 import os
 
+print(",,")
+print(os.getcwd())
+print(",,")
+print(os.listdir())
+
+os.chdir("Compositeurs")
+print("là")
+print(",,")
+print(os.getcwd())
+print(",,")
+print(os.listdir())
 fichiers = set(os.listdir())
 
-fichiers.remove(".git")
-fichiers.remove("README.py")
-fichiers.remove("README.md")
-fichiers.remove("template.md")
+# fichiers.remove(".git")
+# fichiers.remove("README.py")
+# fichiers.remove("README.md")
+# fichiers.remove("template.md")
 
 liste_fichiers = []
 
@@ -26,9 +37,10 @@ texte = f"""# Musique classique
 |-------------------|"""
 
 for comp in liste_fichiers : 
-    texte+= f"""\n|[{comp}](https://github.com/LouisJustinTALLOT/compositeurs/blob/master/{comp}.md)|"""
+    texte += f"""\n|[{comp}](https://github.com/LouisJustinTALLOT/compositeurs/blob/master/{comp}.md)|"""
+texte += "\n"
 
-
+os.chdir(os.path.pardir)
 
 with open('README.md','w', encoding='utf8') as file :
     
