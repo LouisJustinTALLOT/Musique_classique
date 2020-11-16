abd1225700d976598648aafe2f4ad2783a3aef13
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-11-16 15:08:49 +0100

    fonction identifier_ligne(str) pour donner le "type" de la ligne

--- src/structure_de_document.py
+++ src/structure_de_document.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 # print(dir_path)
@@ -10,8 +11,37 @@ def trouver_nom_section(chaine):
     ch = chaine.split(" ")
     return " ".join(ch[1:])
 
-# print(trouver_nom_section("## Concerto pour piano"))
+regex_pattern =  "\A(\|[ a-zA-Z]{5}\w)"
+ma_regex = re.compile(regex_pattern)
 
+def identifier_ligne(ligne):
+    """retourne :
+    1 si une section             ## nom section
+    2 si une sous section        ### nom sous-section
+    3 si une sous sous section   #### nom sous-sous-section
+    4 si c'est un morceau        |Le morceau 
+    -1 sinon
+    """
+
+    if len(ligne) < 6:
+        return -1
+
+    if ma_regex.match(ligne) and ligne[:4] != "|Nom":
+        return 4
+
+    if ligne[:3] == "## ":
+        return 1
+
+    if ligne[:4] == "### ":
+        return 2
+
+    if ligne[:5] == "#### ":   
+        return 3
+
+    return -1
+
+# for li in ["|Symphonie 3|   ", "|Nom de l'oeuvre", "## Symphonies", "### Piano", "#### Scherzos", "  "]:
+#     print(identifier_ligne(li), li)
 
 class SousSousSection :
     def __init__(self,nom_section, txt) : 
