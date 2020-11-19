119a4dec4fc270c8bbeaf8c33c7aca27a629519e
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-11-19 23:44:39 +0100

    on change le mode d'appel à Tkinter #1

--- src/GUI.py
+++ src/GUI.py
@@ -1,7 +1,7 @@
 import os 
 import re
 import structure_de_document as struct
-from tkinter import *
+import tkinter as tk
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -22,19 +22,19 @@ def afficher_doc(doc):
     for widget in fen.winfo_children():
         widget.destroy()
     liste_compo_deroulante(compositeurs)
-    Label(fen, text = "").grid(row = 1, column = 4)
+    tk.Label(fen, text = "").grid(row = 1, column = 4)
     # print(doc.nom)
     # le nom en haut avec le nb de morceaux
     # Label(fen, text = doc.nom).grid(row = 0, column = 3)
-    Label(fen, text = doc.nombre_total_de_morceaux).grid(row = 1, column = 5)
+    tk.Label(fen, text = doc.nombre_total_de_morceaux).grid(row = 1, column = 5)
     # les parties
     for i in range(1,len(doc.liste_sections)+1):
         # un label avec le nom de la section et le nombre d'oeuvres
-        Label(fen, text = doc.liste_sections[i-1].nom).grid(row = i, column = 2)
+        tk.Label(fen, text = doc.liste_sections[i-1].nom).grid(row = i, column = 2)
         if doc.liste_sections[i-1].compter():
-            Label(fen, text = doc.liste_sections[i-1].compter()).grid(row = i, column = 5)
-    Label(fen, text = "").grid(row = i+1, column = 3)
-    Button(fen, text="Quit", command=fen.destroy).grid(row = i+2, column = 3)
+            tk.Label(fen, text = doc.liste_sections[i-1].compter()).grid(row = i, column = 5)
+    tk.Label(fen, text = "").grid(row = i+1, column = 3)
+    tk.Button(fen, text="Quit", command=fen.destroy).grid(row = i+2, column = 3)
 
 def callback_list(*args):
     global var_listbox1
@@ -42,21 +42,21 @@ def callback_list(*args):
 
 def liste_compo_deroulante(compo):
     global var_listbox1
-    var_listbox1 = StringVar(fen)
+    var_listbox1 = tk.StringVar(fen)
     var_listbox1.set("Compositeur")
-    opt = OptionMenu(fen, var_listbox1, *compo)
+    opt = tk.OptionMenu(fen, var_listbox1, *compo)
     var_listbox1.trace("w", callback_list)
     opt.grid(row = 3, column = 0)
     
 # la partie principale
 
-fen = Tk()
+fen = tk.Tk()
 # fen.geometry('500x200')
 # lb = Listbox(fen)
 # lb.insert(1, compositeurs)
 # lb.grid()
 
 liste_compo_deroulante(compositeurs)
-Button(fen, text="Quit", command=fen.destroy).grid(row = 10, column = 3)
+tk.Button(fen, text="Quit", command=fen.destroy).grid(row = 10, column = 3)
 # print(fen.winfo_children())
 fen.mainloop()
