4cf3e5dce0e5f0b5d53f15b2be6bc5a96aaf2eeb
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-11-19 21:03:40 +0100

    on affiche le document en refaisant la fenêtre #1

--- src/GUI.py
+++ src/GUI.py
@@ -17,3 +17,21 @@ compositeurs.sort()
 dict_docs = {}
 for c in compositeurs:
     dict_docs[c] = struct.Document(c)
+
+def afficher_doc(doc):
+    for widget in fen.winfo_children():
+        widget.destroy()
+    liste_compo_deroulante(compositeurs)
+    Label(fen, text = "").grid(row = 1, column = 4)
+    # print(doc.nom)
+    # le nom en haut avec le nb de morceaux
+    # Label(fen, text = doc.nom).grid(row = 0, column = 3)
+    Label(fen, text = doc.tout_compter()).grid(row = 1, column = 5)
+    # les parties
+    for i in range(1,len(doc.liste_sections)+1):
+        # un label avec le nom de la section et le nombre d'oeuvres
+        Label(fen, text = doc.liste_sections[i-1].nom).grid(row = i, column = 2)
+        if doc.liste_sections[i-1].compter():
+            Label(fen, text = doc.liste_sections[i-1].compter()).grid(row = i, column = 5)
+    Label(fen, text = "").grid(row = i+1, column = 3)
+    Button(fen, text="Quit", command=fen.destroy).grid(row = i+2, column = 3)
