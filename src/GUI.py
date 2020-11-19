2d7a2f48eca0a23ef718405c36be1fa81bb6c617
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-11-19 21:03:03 +0100

    on obtient un dictionnaire d'objets Document #1

--- src/GUI.py
+++ src/GUI.py
@@ -8,3 +8,12 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path) # on est dans M_c/src/ 
 os.chdir(os.path.pardir) # dans Musique_classique
 os.chdir("Compositeurs")
+
+compositeurs = os.listdir()
+for i in range(len(compositeurs)):
+    compositeurs[i] = compositeurs[i][:-3]
+compositeurs.sort()
+
+dict_docs = {}
+for c in compositeurs:
+    dict_docs[c] = struct.Document(c)
