75b11fdc0f035ae092963dcd4f271ac00eb75570
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-11-19 21:02:16 +0100

    accès au dossier où se trouvent les fiches compositeurs #1

--- src/GUI.py
+++ src/GUI.py
@@ -3,3 +3,8 @@ import re
 import structure_de_document as struct
 from tkinter import *
 
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path) # on est dans M_c/src/ 
+os.chdir(os.path.pardir) # dans Musique_classique
+os.chdir("Compositeurs")
